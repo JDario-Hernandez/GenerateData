d1fbed349835cdf62353768855f04effcc3d8873
LorenaOB <149644688+LorenaOB@users.noreply.github.com>
2024-03-25 21:09:36 -0500

    Update and rename generate.java to GenerateInfoFiles.java
    Rename class

--- src/main/GenerateInfoFiles.java
+++ src/main/GenerateInfoFiles.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 import model.Product;
 import service.generateData;
 
-public class generate {
+public class GenerateInfoFiles {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
