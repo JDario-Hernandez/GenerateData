10b0fe44036e34e32aec5d0a08bf2313bfee517a
Lorena Ortiz <lorenaortizb@gmail.com>
2024-04-15 13:14:04 -0500

    Modification of class generateData

--- src/service/generateData.java
+++ src/service/generateData.java
@@ -5,40 +5,83 @@ import java.util.List;
 
 import model.Product;
 import model.Seller;
+import model.Sale;
 
 public class generateData {
 	private List<Seller> sellers;
-	private List<Product> productos;
+	private List<Product> products;
+	private List<Sale> sales;
 
 	public generateData() {
 		sellers = new ArrayList<Seller>();
-		productos = new ArrayList<Product>();
+		products = new ArrayList<Product>();
+		sales = new ArrayList<Sale>();
 	}
 
 	public List<Seller> getSellers() {
 		return sellers;
 	}
 
-	public List<Product> getProductos() {
-		return productos;
+	public List<Product> getProducts() {
+		return products;
+	}
+	
+	public List<Sale> getSales() {
+		return sales;
 	}
 
 	public void addSeller(int id, String name, String lastname, String document) {
-		Seller seller = new Seller();
-		seller.setId(id);
-		seller.setName(name);
-		seller.setLastname(lastname);
-		seller.setDocument(document);
+		Seller seller = new Seller(id, name, lastname, document);
 		sellers.add(seller);
 	}
 
-	public void addProductoSold(int id, String name, int price, int idSeller) {
-		Product product = new Product();
-		product.setId(id);
-		product.setIdSeller(idSeller);
-		product.setName(name);
-		product.setPrice(price);
-		productos.add(product);
+	public void addProduct(int id, String name, int price) {
+		Product product = new Product(id, name, price);
+		products.add(product);
+	}
+	
+	public void addSale(int idSeller, String nameProduct, int quantity) {
+		if (quantity == 0) return; // Si la cantidad de la venta fue 0, no se registra
+		
+		Product product = getProductByName(nameProduct);
+		Seller seller = getSellerById(idSeller);
+		
+		// Buscar si el vendedor ya realizó una venta previa de ese producto
+		for (Sale sale: sales ) {
+			if (sale.getSeller().equals(seller) && sale.getProduct().equals(product)) {
+				sale.setQuantity(sale.getQuantity() + quantity);
+				sale.setCollected(sale.getCollected() + (quantity*product.getPrice()));
+				return;
+			}
+		}
+		
+		Sale sale = new Sale(product, seller, quantity);
+		sale.setQuantity(quantity);
+		sale.setCollected(quantity*product.getPrice());
+		
+		sales.add(sale);
+	}
+	
+	private Product getProductByName(String nameProduct) {
+		for (Product product: products)	{
+			if (product.getName().equals(nameProduct)) {
+				return product;
+			}
+		}
+		
+		Product product = new Product(products.size(), nameProduct, 500000); // TODO: El precio del producto está estático
+		return product;
+	}
+	
+	private Seller getSellerById(int idSeller) {
+		for (Seller seller: sellers)	{
+			if (seller.getId() == idSeller) {
+				return seller;
+			}
+		}
+		
+		return null;
 	}
 
 }
+
